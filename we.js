7ec6c4523676e68717f58887eec96213d6a4f7c9
khonzinkhosim-sketch <khonzinkhosim@gmail.com>
2026-06-23 22:50:18 +0200

    Add files via upload

---
+++ we.js
@@ -0,0 +1,37 @@
+﻿window.onload = function(){
+    alert("Welcome to Mkhondvo High School Archive Website");
+};
+
+function updateDateTime(){
+    const now = new Date();
+    document.getElementById("datetime").innerHTML =
+        now.toLocaleString();
+}
+
+setInterval(updateDateTime, 1000);
+updateDateTime();
+
+function searchMemories(){
+
+    let input =
+        document.getElementById("searchBox")
+        .value
+        .toLowerCase();
+
+    let memories =
+        document.querySelectorAll(".memory");
+
+    memories.forEach(memory => {
+
+        if(memory.textContent
+            .toLowerCase()
+            .includes(input)){
+
+            memory.style.display = "block";
+
+        }else{
+
+            memory.style.display = "none";
+        }
+    });
+}
